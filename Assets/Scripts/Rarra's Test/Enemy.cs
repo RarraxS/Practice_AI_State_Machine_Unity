0e92bf4d79b4b17d86be363287456beeceb39674
RarraxS <itisrarra@gmail.com>
2024-11-25 05:10:15 +0100

    Implementación de la percepción del personaje (parte 19
    Se ha empezado a implementar el raycast que permitirá hacer que el enemigo "vea" a nuestro personaje.

---
+++ Assets/Scripts/Rarra's Test/Enemy.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.UI.Image;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float maxWatchDistance;
+
+    private Transform tr;
+
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Watch();
+
+        //Move(player);
+    }
+
+    private void Move(Transform target)
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void Watch()
+    {
+        Vector3 start = tr.position + new Vector3(0, 0.2f, 0);
+
+        Vector3 end = player.position + new Vector3(0, 0.2f, 0);
+
+        Vector3 direction = (start - end).normalized;
+
+        float distance = Vector3.Distance(start, end);
+
+        Physics.Raycast(start, direction, out RaycastHit hit, distance);
+        Debug.DrawRay(tr.position, player.position * maxWatchDistance, Color.black);
+
+        //if (Physics.Raycast(start, direction, out RaycastHit hit, distance))
+        //{
+        //    // Si el rayo golpea algo
+        //    Debug.Log($"El rayo impactó con: {hit.collider.name}");
+
+        //    // Visualización opcional del rayo en la escena
+        //    Debug.DrawLine(tr.position, hit.point, Color.red);
+
+        //    Debug.DrawRay(tr.position, player.position * maxWatchDistance, Color.black);
+
+        //}
+
+
+
+
+        //Physics.Raycast(tr.position, direction, out RaycastHit hit, distance);
+        //Debug.DrawLine(tr.position, hit.point, Color.red);
+
+
+        //Vector3 origen = transform.position;
+        //Vector3 direccion = Vector3.forward;
+        //float distancia = 12f;
+
+        //RaycastHit2D hit = Physics2D.Raycast(origen, direccion, distancia);
+        //Debug.DrawRay(origen, direccion * distancia, Color.red);
+
+
+
+        ////Physics.Raycast(tr.position, player, out RaycastHit hit, maxWatchDistance);
+        //Ray ray = new Ray(transform.position, player.transform.position);
+        //Debug.DrawRay(transform.position, player.transform.position * maxWatchDistance, Color.red);
+    }
+}
